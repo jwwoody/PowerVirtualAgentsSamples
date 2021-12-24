1be4caf98a27bb4aa10209c69f76d46dd8c064a4
jwwoody <51487814+jwwoody@users.noreply.github.com>
2021-12-23 19:01:35 -0500

    Update generateDirectLineToken.js

--- BuildYourOwnCanvasSamples/2.location-and-file-uploading/web/src/generateDirectLineToken.js
+++ BuildYourOwnCanvasSamples/2.location-and-file-uploading/web/src/generateDirectLineToken.js
@@ -7,9 +7,15 @@ module.exports = async function generateDirectLineToken(botId, botTenantId) {
   
   // The URL host in which to generate the Direct Line token is subject to change based off of environment.
   // The host should be consistent to the host that you are building your Power VA.
-  const response = await fetch('https://va.ai.dynamics.com/api/botmanagement/v1/directline/directlinetoken?botId=' + botId + '&tenantId=' + botTenantId, {method: "GET"});
+  // const response = await fetch('https://va.ai.dynamics.com/api/botmanagement/v1/directline/directlinetoken?botId=' + botId + '&tenantId=' + botTenantId, {method: "GET"});
+  console.log('BOT ID: ' + botId);
+  console.log('BOT Tenant ID: ' + botTenantId);
+  Secret = 'JVmE88ln1qU.sP46ls1yjXX9uHd1y3NxYfOMK1cFkrmKlnnfaPfk1Bs'
+  const response = await fetch('https://powerva.microsoft.com/api/botmanagement/v1/directline/directlinetoken?botId=' +botId + '&tenantId=' + botTenantId,{method: "POST", headers: {'Authorization': 'Bearer JVmE88ln1qU.sP46ls1yjXX9uHd1y3NxYfOMK1cFkrmKlnnfaPfk1Bs'}});
+
   
   const { token } = await response.json();
-
+  console.log('Token: ' + token);
   return token;
+  
 };
